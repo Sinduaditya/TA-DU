6f1a8c8abe2140a3922e962df134e968b21ba6e2
SinduAditya <nduujanadi51@gmail.com>
2024-04-12 20:04:55 +0700

    setup ta

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center text-red-800 underline">
+      Hello Selamat Mengerjakan DU!
+    </h1>
+  )
+}
